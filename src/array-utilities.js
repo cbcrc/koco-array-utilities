ecf84b28c75d1a85a142c4fb5f66841805c78f62
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:19:13 -0400

    update to koco2

--- src/array-utilities.js
+++ src/array-utilities.js
@@ -1,47 +1,42 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define([],
-    function() {
-        'use strict';
+var ArrayUtilities = function() {};
 
-        var ArrayUtilities = function() {};
+ArrayUtilities.prototype.isArray = function(object) {
+    return Object.prototype.toString.call(object) === '[object Array]';
+};
 
-        ArrayUtilities.prototype.isArray = function(object) {
-            return Object.prototype.toString.call(object) === '[object Array]';
-        };
+ArrayUtilities.prototype.isEmptyArray = function(object) {
+    var self = this;
+    var result = false;
 
-        ArrayUtilities.prototype.isEmptyArray = function(object) {
-            var self = this;
-            var result = false;
+    if (self.isArray(object)) {
+        if (object.length === 0) {
+            result = true;
+        }
+    }
 
-            if (self.isArray(object)) {
-                if (object.length === 0) {
-                    result = true;
-                }
-            }
+    return result;
+};
 
-            return result;
-        };
+ArrayUtilities.prototype.isEmptyArrayOrNull = function(object) {
+    var self = this;
+    var result = true;
 
-        ArrayUtilities.prototype.isEmptyArrayOrNull = function(object) {
-            var self = this;
-            var result = true;
+    if (self.isArray(object)) {
+        if (object.length > 0) {
+            result = false;
+        }
+    }
 
-            if (self.isArray(object)) {
-                if (object.length > 0) {
-                    result = false;
-                }
-            }
+    return result;
+};
 
-            return result;
-        };
+ArrayUtilities.prototype.isNotEmptyArray = function(object) {
+    var self = this;
 
-        ArrayUtilities.prototype.isNotEmptyArray = function(object) {
-            var self = this;
+    return object !== null && self.isArray(object) && !self.isEmptyArray(object);
+};
 
-            return object !== null && self.isArray(object) && !self.isEmptyArray(object);
-        };
-
-        return new ArrayUtilities();
-    });
+export default new ArrayUtilities();
